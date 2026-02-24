ce4725b886fa710bd0a038352e714a64328f7c22
Sanababayan <babayansana@gmail.com>
2026-02-25 00:32:58 +0330

    test setting updated

--- Backend/server/test_settings.py
+++ Backend/server/test_settings.py
@@ -1,4 +1,10 @@
-from .settings import *
+import os
+from pathlib import Path
+from .settings import *  # Import all base settings
+
+# Override settings for tests
+DEBUG = True
+SECRET_KEY = 'django-insecure-test-key-123456789'
 
 # Use in-memory SQLite for tests
 DATABASES = {
@@ -16,8 +22,29 @@ PASSWORD_HASHERS = [
 # Use console email backend for tests
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
-# Disable debug
-DEBUG = False
+# Disable S3 storage for tests
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+    }
+}
 
-# Use test static files
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# Create test media directory
+TEST_MEDIA_ROOT = BASE_DIR / 'test_media'
+TEST_MEDIA_ROOT.mkdir(exist_ok=True)
+MEDIA_ROOT = str(TEST_MEDIA_ROOT)
+MEDIA_URL = '/media/'
+
+# Disable security features for tests
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
+# Override any config settings that might cause issues
+# This ensures we don't rely on the conf object for tests
+if 'conf' in locals():
+    # Just ignore conf for tests
+    pass
